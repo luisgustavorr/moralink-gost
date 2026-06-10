2e01ccffe9673385ee480dc9c8ecbf8da2cc0edd
luisgustavorr <luisgustavo20061@gmail.com>
2026-06-10 17:22:50 -0300

    updating to v0.0.9

--- modules/utils/util.go
+++ modules/utils/util.go
@@ -17,7 +17,7 @@ import (
 	"google.golang.org/protobuf/types/known/structpb"
 )
 
-var Version string = "v0.0.8"
+var Version string = "v0.0.9"
 
 type CategoriaRow struct {
 	IdExterno *string `db:"id_externo"`
